88b750f64ad2df87592cb9f997fdf055e288e12f
Miłosz Pawłowski <milosz.pawlowski.torun@wp.pl>
2022-10-09 22:01:45 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 let scrollTopValue = 0;
-const x = document.getElementById("aboutText");
+const x = document.getElementById("aboutText").position().top;
+let y = window.screen.height -100;
 
 window.addEventListener("scroll", (e) => {
     scrollTopValue = window.scrollY; 
     console.log(x.getBoundingClientRect().top + window.scrollY);
     console.log("Y:" + scrollTopValue);
+    console.log("screen height: " + y);
 });
